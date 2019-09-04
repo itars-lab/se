9bc0158f2cd7e3d9a547c1a5b82ecc873024703b
Pawel Tarsa <itars.lab@gmail.com>
2019-09-04 23:19:43 +0200

    Homeworkg: doubleClick method implementation

--- src/test/java/pl/qa/selenium/pages/BasePage.java
+++ src/test/java/pl/qa/selenium/pages/BasePage.java
@@ -1,11 +1,19 @@
 package pl.qa.selenium.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.function.Function;
 
 public class BasePage {
 
@@ -21,6 +29,7 @@ public class BasePage {
         this.fluentWait = new FluentWait(driver)
                 .ignoring(NoSuchElementException.class)
                 .withTimeout(Duration.ZERO);
+        this.visit();
     }
 
     public String getTitle() {
@@ -33,6 +42,95 @@ public class BasePage {
     public BasePage visit() {
         this.driver.get(pageUrl);
         return this;
+    }
+
+    public WebElement find(By locator) {
+        return this.driver.findElement(locator);
+    }
+
+    public WebElement findFileSafe(By locator) {
+        List<WebElement> elements = this.driver.findElements(locator);
+        if (elements.isEmpty()) {
+            return null;
+        }
+        return elements.get(0);
+    }
+
+    public BasePage click(By locator) {
+        this.find(locator).click();
+        return this;
+    }
+
+    public BasePage clickFailSafe(By locator) {
+        List<WebElement> elements = this.driver.findElements(locator);
+        if (elements.isEmpty()) {
+            return this;
+        }
+        elements.get(0).click();
+        return this;
+    }
+
+    public Boolean waitFor(Function<WebDriver, Boolean> expectedCondition) {
+        return (Boolean) fluentWait.until(expectedCondition);
+    }
+
+    public void waitFor(ExpectedCondition<WebElement> expectedCondition, Integer timoeout) {
+        new WebDriverWait(driver, timoeout).until(expectedCondition);
+    }
+
+    public WebElement hover(By locator) {
+        Actions actions = new Actions(driver);
+        WebElement target = this.find(locator);
+        actions.moveToElement(target).perform();
+        return target;
+    }
+
+
+    /**
+     * @param input
+     * @param locator
+     */
+    public void type(String input, By locator) {
+        find(locator).clear();
+        find(locator).sendKeys(input);
+    }
+
+    public void submit(By locator) {
+        find(locator).submit();
+    }
+
+    public String getTextOf(By locator) {
+        return find(locator).getText();
+    }
+
+    /**
+     * @param locator
+     * @return
+     */
+    public Boolean isDisplayed(By locator) {
+        try {
+            return find(locator).isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    /**
+     * @param locator
+     * @param timeout
+     * @return
+     */
+    public Boolean waitForIsDisplayed(By locator, Integer timeout) {
+        try {
+            waitFor(ExpectedConditions.visibilityOfElementLocated(locator), timeout);
+        } catch (TimeoutException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public void doubleClick(By locator) {
+        // todo You Actions class
     }
 
 }
--- src/test/java/pl/qa/selenium/tests/LandingPageTest.java
+++ src/test/java/pl/qa/selenium/tests/LandingPageTest.java
@@ -3,7 +3,12 @@ package pl.qa.selenium.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import pl.qa.selenium.pages.LoadingPage;
+
+import java.util.regex.Pattern;
 
 public class LandingPageTest extends BaseTest {
 
@@ -25,7 +30,22 @@ public class LandingPageTest extends BaseTest {
     @Test
     public void searchTestCmeGroup() throws InterruptedException {
         page.search("test cme selenium");
+        page.find(By.cssSelector("sdjahsjkdhasd"));
         Assertions.assertEquals(page.getTitle(), "CME group");
+    }
+
+    @Test
+    public void goToPortfolio() {
+        By contextMenuArrow = By.cssSelector("#cmeMenuLogin > a");
+        By xpath = By.xpath("//div[@id='cmeMenu']//li/a[text()='Login']");
+        page.find(xpath);
+        WebElement menu = page.find(contextMenuArrow);
+        page.hover(xpath);
+        WebElement myPortfolio = menu.findElement(By.xpath("//a[text()='My Portfolio']"));
+        //todo debug it
+//        menu.findElements(By.xpath("//*")).stream().map(WebElement::getText).collect(Collectors.toList())
+        myPortfolio.getText();
+        page.waitFor(ExpectedConditions.textMatches(By.xpath(""), Pattern.compile("")));
     }
 
 
